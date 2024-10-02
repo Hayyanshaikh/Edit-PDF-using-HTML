ce4cc3b2be5fd2926de2f24cf041f92525735aff
Hayyanshaikh <git@stackblitz.com>
2024-10-02 22:58:27 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Edit-PDF-using-HTML
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Hayyanshaikh/Edit-PDF-using-HTML)
---
+++ script.js
@@ -0,0 +1,488 @@
+// Guest Info Data
+const guestInfo = {
+  name: 'Muhammad Hamza Ali',
+  phone: '+92 3150229667',
+  email: 'Hamza.ali180221@gmail.com',
+  city: 'Karachi',
+  country: 'Pakistan',
+};
+
+// Package Info Data
+const packageInfo = {
+  generatedDate: '30-08-2024',
+  validTill: '7 days',
+  startDate: '01 Sep, 24',
+  endDate: '10 Sep, 24',
+  adults: 3,
+  children: 2,
+  packageType: 'Economy',
+  price: '$235',
+};
+
+// Activities Data
+const activities = [
+  {
+    date: 'Monday, August 15, 2024',
+    islamicDate: 'Safar 1446 AH 1',
+    event: 'Arbaeen',
+    stay: 'Night Stay: Najaf',
+    places: [
+      {
+        title: 'Haram E Imam Hussain A.s',
+        city: 'Karbala',
+        travel_by: 'bus',
+        travel_time: {
+          cost: 30,
+          value: 180,
+          label: '150km - 3 hrs by bus',
+        },
+        stay_time: '2 hrs Stay time',
+        cost_per_person: 30,
+        separator: true,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Hazrat Abbas A.s',
+        city: 'Karbala',
+        travel_by: 'walk',
+        travel_time: {
+          cost: 0,
+          value: 10,
+          label: '10 min walk',
+        },
+        stay_time: '1:45 hrs Stay time',
+        cost_per_person: 0,
+        separator: false,
+        dateNow: 1727880796629,
+      },
+    ],
+  },
+  {
+    date: 'Monday, August 15, 2024',
+    islamicDate: 'Safar 1446 AH 1',
+    event: 'Arbaeen',
+    stay: 'Night Stay: Najaf',
+    places: [
+      {
+        title: 'Haram E Imam Hussain A.s',
+        city: 'Karbala',
+        travel_by: 'bus',
+        travel_time: {
+          cost: 30,
+          value: 180,
+          label: '150km - 3 hrs by bus',
+        },
+        stay_time: '2 hrs Stay time',
+        cost_per_person: 30,
+        separator: true,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Hazrat Abbas A.s',
+        city: 'Karbala',
+        travel_by: 'walk',
+        travel_time: {
+          cost: 0,
+          value: 10,
+          label: '10 min walk',
+        },
+        stay_time: '1:45 hrs Stay time',
+        cost_per_person: 0,
+        separator: false,
+        dateNow: 1727880796629,
+      },
+    ],
+  },
+  {
+    date: 'Monday, August 15, 2024',
+    islamicDate: 'Safar 1446 AH 1',
+    event: 'Arbaeen',
+    stay: 'Night Stay: Najaf',
+    places: [
+      {
+        title: 'Haram E Imam Hussain A.s',
+        city: 'Karbala',
+        travel_by: 'bus',
+        travel_time: {
+          cost: 30,
+          value: 180,
+          label: '150km - 3 hrs by bus',
+        },
+        stay_time: '2 hrs Stay time',
+        cost_per_person: 30,
+        separator: true,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Hazrat Abbas A.s',
+        city: 'Karbala',
+        travel_by: 'walk',
+        travel_time: {
+          cost: 0,
+          value: 10,
+          label: '10 min walk',
+        },
+        stay_time: '1:45 hrs Stay time',
+        cost_per_person: 0,
+        separator: false,
+        dateNow: 1727880796629,
+      },
+    ],
+  },
+  {
+    date: 'Monday, August 15, 2024',
+    islamicDate: 'Safar 1446 AH 1',
+    event: 'Arbaeen',
+    stay: 'Night Stay: Najaf',
+    places: [
+      {
+        title: 'Haram E Imam Hussain A.s',
+        city: 'Karbala',
+        travel_by: 'bus',
+        travel_time: {
+          cost: 30,
+          value: 180,
+          label: '150km - 3 hrs by bus',
+        },
+        stay_time: '2 hrs Stay time',
+        cost_per_person: 30,
+        separator: true,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Hazrat Abbas A.s',
+        city: 'Karbala',
+        travel_by: 'walk',
+        travel_time: {
+          cost: 0,
+          value: 10,
+          label: '10 min walk',
+        },
+        stay_time: '1:45 hrs Stay time',
+        cost_per_person: 0,
+        separator: false,
+        dateNow: 1727880796629,
+      },
+    ],
+  },
+  {
+    date: 'Monday, August 15, 2024',
+    islamicDate: 'Safar 1446 AH 1',
+    event: 'Arbaeen',
+    stay: 'Night Stay: Najaf',
+    places: [
+      {
+        title: 'Haram E Imam Hussain A.s',
+        city: 'Karbala',
+        travel_by: 'bus',
+        travel_time: {
+          cost: 30,
+          value: 180,
+          label: '150km - 3 hrs by bus',
+        },
+        stay_time: '2 hrs Stay time',
+        cost_per_person: 30,
+        separator: true,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Hazrat Abbas A.s',
+        city: 'Karbala',
+        travel_by: 'walk',
+        travel_time: {
+          cost: 0,
+          value: 10,
+          label: '10 min walk',
+        },
+        stay_time: '1:45 hrs Stay time',
+        cost_per_person: 0,
+        separator: false,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Imam Hussain A.s',
+        city: 'Karbala',
+        travel_by: 'bus',
+        travel_time: {
+          cost: 30,
+          value: 180,
+          label: '150km - 3 hrs by bus',
+        },
+        stay_time: '2 hrs Stay time',
+        cost_per_person: 30,
+        separator: true,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Hazrat Abbas A.s',
+        city: 'Karbala',
+        travel_by: 'walk',
+        travel_time: {
+          cost: 0,
+          value: 10,
+          label: '10 min walk',
+        },
+        stay_time: '1:45 hrs Stay time',
+        cost_per_person: 0,
+        separator: false,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Imam Hussain A.s',
+        city: 'Karbala',
+        travel_by: 'bus',
+        travel_time: {
+          cost: 30,
+          value: 180,
+          label: '150km - 3 hrs by bus',
+        },
+        stay_time: '2 hrs Stay time',
+        cost_per_person: 30,
+        separator: true,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Hazrat Abbas A.s',
+        city: 'Karbala',
+        travel_by: 'walk',
+        travel_time: {
+          cost: 0,
+          value: 10,
+          label: '10 min walk',
+        },
+        stay_time: '1:45 hrs Stay time',
+        cost_per_person: 0,
+        separator: false,
+        dateNow: 1727880796629,
+      },
+    ],
+  },
+  {
+    date: 'Wednesday, August 17, 2024',
+    islamicDate: 'Safar 1446 AH 3',
+    event: 'Arbaeen Procession',
+    stay: 'Night Stay: Kazmain',
+    places: [
+      {
+        title: 'Haram E Imam Musa Kazim A.s',
+        city: 'Kazmain',
+        travel_by: 'bus',
+        travel_time: {
+          cost: 20,
+          value: 120,
+          label: '100km - 2 hrs by bus',
+        },
+        stay_time: '3 hrs Stay time',
+        cost_per_person: 20,
+        separator: true,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Imam Jawad A.s',
+        city: 'Kazmain',
+        travel_by: 'walk',
+        travel_time: {
+          cost: 0,
+          value: 5,
+          label: '5 min walk',
+        },
+        stay_time: '1 hr Stay time',
+        cost_per_person: 0,
+        separator: false,
+        dateNow: 1727880796629,
+      },
+    ],
+  },
+  {
+    date: 'Friday, August 20, 2024',
+    islamicDate: 'Safar 1446 AH 6',
+    event: 'Ziyarat',
+    stay: 'Night Stay: Samarra',
+    places: [
+      {
+        title: 'Haram E Imam Ali Naqi A.s',
+        city: 'Samarra',
+        travel_by: 'bus',
+        travel_time: {
+          cost: 40,
+          value: 240,
+          label: '200km - 4 hrs by bus',
+        },
+        stay_time: '2 hrs Stay time',
+        cost_per_person: 40,
+        separator: true,
+        dateNow: 1727880796629,
+      },
+      {
+        title: 'Haram E Imam Hassan Askari A.s',
+        city: 'Samarra',
+        travel_by: 'walk',
+        travel_time: {
+          cost: 0,
+          value: 10,
+          label: '10 min walk',
+        },
+        stay_time: '1:30 hrs Stay time',
+        cost_per_person: 0,
+        separator: false,
+        dateNow: 1727880796629,
+      },
+    ],
+  },
+];
+
+// Function to create table rows for guest info
+function createRow(label, value) {
+  return `
+    <td>
+      <div class="table-content">
+        <label>${label}</label>
+        <p>${value}</p>
+      </div>
+    </td>
+  `;
+}
+
+// Creating Guest Info Table
+const guestTable = `
+  <table>
+    <thead>
+      <tr>
+        <th colspan="5">Guest info</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        ${createRow('Name', guestInfo.name)}
+        ${createRow('Phone', guestInfo.phone)}
+        ${createRow('Email Address', guestInfo.email)}
+        ${createRow('City', guestInfo.city)}
+        ${createRow('Country', guestInfo.country)}
+      </tr>
+    </tbody>
+  </table>
+`;
+
+// Creating Package Info Table
+const packageTable = `
+  <table>
+    <thead>
+      <tr>
+        <th colspan="3">Generated date: ${packageInfo.generatedDate}</th>
+        <th colspan="3">The quoted rates are only valid till ${
+          packageInfo.validTill
+        }</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        ${createRow('Start Date', packageInfo.startDate)}
+        ${createRow('End Date', packageInfo.endDate)}
+        ${createRow('Adults', packageInfo.adults)}
+        ${createRow('Children', packageInfo.children)}
+        ${createRow('Package', packageInfo.packageType)}
+        ${createRow('Price', packageInfo.price)}
+      </tr>
+    </tbody>
+  </table>
+`;
+
+// Creating Activities Table Dynamically Based on the New Structure
+const activityTables = activities
+  .map((activity, index) => {
+    let placesHTML = activity.places
+      .map((place) => {
+        let travelTimeHTML = place.travel_time
+          ? `<p>${place.travel_time.label}</p>`
+          : '';
+
+        let stayTimeHTML = place.stay_time ? `<p>${place.stay_time}</p>` : '';
+
+        let separatorHTML = place.separator
+          ? `
+        <tr>
+          <td colSpan="2">
+            <div class="table-content">
+                <div class="trip-place row">
+                  <h3 class="place-name">${place.city} to ${place.title} Travel</h3>
+                  <div class="table-content row gap">
+                    <p>${place.travel_by}</p>
+                    <p>${place.travel_time.label}</p>
+                  </div>
+                </div>
+              </div>
+            </td>  
+        </tr>
+        `
+          : '';
+
+        return `
+          <tr>
+            <td colspan="2">
+              <div class="table-content">
+                <div class="trip-place">
+                  <h3 class="place-name">${place.title}</h3>
+                  <div class="table-content row gap">
+                    <p> ${place.city || ''}</p>
+                    <p>${place.travel_by || ''}</p>
+                    ${travelTimeHTML}
+                    ${stayTimeHTML}
+                    <p>$${place.cost_per_person}</p>
+                  </div>
+                </div>
+              </div>
+              ${separatorHTML}
+            </td>
+          </tr>
+        `;
+      })
+      .join('');
+
+    return `
+    <table>
+        ${
+          index === 0
+            ? `
+        <thead>
+          <tr>
+            <th colspan="2">Activities</th>
+          </tr>
+        </thead>
+        `
+            : ''
+        }
+        <tbody>
+          <tr class="${index === activities.length - 1 ? '' : 'page-break'}">
+            <td>
+              <div class="table-content">
+                <div class="table-content row gap">
+                  <p>${activity.date}</p>
+                  <p>${activity.islamicDate}</p>
+                  <p>${activity.event}</p>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div class="table-content">
+                <p class="text-right">${activity.stay}</p>
+              </div>
+            </td>
+          </tr>
+          ${placesHTML}
+        </tbody>
+      </table>
+    `;
+  })
+  .join('');
+
+// Adding everything to the PDF container
+document.getElementById('pdf-content').innerHTML = `
+  <div class="logo">
+    <img src="./images/logo.svg" alt="Logo" />
+  </div>
+  ${guestTable}
+  ${packageTable}
+  ${activityTables}
+`;
+
+window.print();
